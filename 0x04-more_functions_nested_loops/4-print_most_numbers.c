b2453f55cbe4b90c9a428633dacfdd74bdf36de1
MSZAKARIA1 <zakariamahamasaani@gmail.com>
2022-09-16 09:59:57 -0700

    print number from 0-1 and skip 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_most_number: - function print numbers from 0-1
+ *function: -should not print 2 & 4
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+	if (i == 50 || i == 52)
+	{
+		continue;
+	}
+	else
+	{
+		putchar("%d", i);
+	}
+	}
+		i++;
+
+		putchar("\n");
+}
