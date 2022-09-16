17f46586d28463113addb1354b90ccac6f72d9d4
MSZAKARIA1 <zakariamahamasaani@gmail.com>
2022-09-16 08:53:26 -0700

    loop through number from 0 to 9 & print on a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers: -it loops through number from 0 -9
+ *@i: -initialization
+ *Returns: void
+ */
+void print_numbers(void)
+{	
+	for (int i = 0, i <= 9, i++);
+
+	putchar(i);
+	putchar(\n);
+}
