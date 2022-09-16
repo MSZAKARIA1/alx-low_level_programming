ddd0c8edf85338f90744a0aaca9f353a13b3b80d
MSZAKARIA1 <zakariamahamasaani@gmail.com>
2022-09-16 07:41:17 -0700

    program multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul: - The fnx multiply two integers
+ *@a: firt integer value
+ *@b: second integer value
+ *Return: - Returns the product of a and b
+ */
+int mul(int a, int b)
+{
+	int a, b;
+
+	result = a * b;
+	return (result);
+}
