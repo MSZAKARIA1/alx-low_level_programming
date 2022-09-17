35a62fce02cb82f9a363184a57efea625ea8a176
MSZAKARIA1 <zakariamahamasaani@gmail.com>
2022-09-17 05:46:35 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * print_line - function that prints line on the terminal
+  *
+  * @n: length of the line
+
+  * Return: Always return 0
+  *
+  */
+void print_line(int n)
+{
+	int line;
+
+	line = 0;
+
+	if (n > 0)
+	{
+		while (line < n)
+		{
+		_putchar('_');	
+		
+		line++;
+		}
+	}
+	_putchar(10);
+}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 /**
- *
  * _putchar - writes the character c to stdout
  *
  *@c: The character to print
@@ -11,10 +10,10 @@
  *
  * On error, -1 is returned, and errno is set appropriately.
  *
- * */
+ **/
 
 int _putchar(char c)
-{	
+{
 	return (write(1, &c, 1));
 
 }
