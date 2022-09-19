c8b63ead5b8b3ba1b30c4c4fe04e339b1cdad15d
MSZAKARIA1 <zakariamahamasaani@gmail.com>
2022-09-19 10:32:55 -0700

    A function that returns the lenght of a string

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ *swap_int: -swaps two numbersusing pointers
+ *string _putchar()
+ *
+ *Return: 0 if successful
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+
+	a =
+}
