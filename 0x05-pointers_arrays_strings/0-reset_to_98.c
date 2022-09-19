dd93cd414d7f101f77ba6d8fd8e75556489cbf8c
MSZAKARIA1 <zakariamahamasaani@gmail.com>
2022-09-19 09:59:07 -0700

    basic pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include " maian.h"
+
+/**
+ * reset_to_98 -takes a pointer and an int as parameter
+ * and update the value and point it to 98
+ * @n: -pointer to int n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
