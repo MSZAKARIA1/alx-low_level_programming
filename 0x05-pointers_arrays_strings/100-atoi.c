c3a506316e714d40196a00681cec5c5d6fbbc032
MSZAKARIA1 <zakariamahamasaani@gmail.com>
2022-09-19 16:40:14 -0700

    A function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include "main.h"
 /**
- * main - program that generates random valid
- * passwords for the program 101-crackme
- * Return: Always 0 (Success)
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
  */
-int main(void)
+int _atoi(char *s)
 {
-	int pwd[100];
+	int i, d, n, len, f, digit;
 
-	int i, sum, n;
+	i = 0;
 
-	sum = 0;
-	srand(time(NULL));
-	for (i = 0; i < 100; i++)
+	d = 0;
+
+	n = 0;
+
+	len = 0;
+
+	f = 0;
+
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		pwd[i] = rand() % 78;
-		sum += (pwd[i] + '0');
-		putchar(pwd[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
+		i++;
 	}
-
-	return (0);
+	if (f == 0)
+		return (0);
+	return (n);
 }
