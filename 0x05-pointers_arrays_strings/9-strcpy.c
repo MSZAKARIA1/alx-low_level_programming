3441e903cc3f0200415f6f5f23e437a30e56791f
MSZAKARIA1 <zakariamahamasaani@gmail.com>
2022-09-19 16:30:22 -0700

    A function that copies

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,47 +9,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-
 	int len, i;
 
-
-
 	len = 0;
-
-
-
 	while (src[len] != '\0')
-
 	{
-
 		len++;
-
 	}
-
-
-
 	for (i = 0; i < len; i++)
-
 	{
-
 		dest[i] = src[i];
-
 	}
-
 	dest[i] = '\0';
-
-
-
 	return (dest);
-
 }
-
-Footer
-
-© 2022 GitHub, Inc.
-
-Footer navigation
-
-Terms
-
-
