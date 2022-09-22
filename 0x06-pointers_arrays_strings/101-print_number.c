a4eaa24b7dfca394dcdf629c7711c96f5c744425
MSZAKARIA1 <zakariamahamasaani@gmail.com>
2022-09-22 15:31:51 -0700

    A function that prints numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,27 +1,22 @@
-#include <stdio.h>
-int main(void)
+#include "main.h"
+/**
+ * print_number - prints an integer;
+ * @n: integer to be printed;
+ */
+void print_number(int n)
 {
-  int n;
+	unsigned int n1;
 
-  int a[5];
+	if (n < 0)
+	{
+		n1 = -n;
 
-  int *p;
-
-  a[2] = 1024;
-
-  p = &n;
-
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
 }
